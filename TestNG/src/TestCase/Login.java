2fd5dbd369bc82f2f2cf175d0456e2766fff264f
ramyapraga14 <ramya.sethu20@gmail.com>
2019-01-30 18:24:26 +0600

    changes

--- TestNG/src/TestCase/Login.java
+++ TestNG/src/TestCase/Login.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import PageObjects.HomePage;
@@ -27,13 +28,21 @@ public class Login {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://magento.com");
 	}
-	@Test(priority=10, groups= {"R","S"})
-	public void Negative_login() {
+	@Test(priority=10, groups= {"R","S"}, dataProvider="negative_data")
+	public void Negative_login(String email, String pwd) {
 		driver.findElement(HomePage.user_icon).click();
-		driver.findElement(LoginPage.email_ip).sendKeys("ABCXYZ@gmail.com");
-		driver.findElement(LoginPage.pwd_ip).sendKeys("welcome123");
+		driver.findElement(LoginPage.email_ip).sendKeys(email);
+		driver.findElement(LoginPage.pwd_ip).sendKeys(pwd);
 		driver.findElement(LoginPage.sign_in_buttn).click();
 		Assert.assertEquals(driver.findElement(UserPage.error_msg).getText(), "Invalid login or password.");
+	}
+	@DataProvider
+	public Object[][] negative_data(){
+		return new Object[][] {
+			{"email1@gmail.com","password"},
+			{"email2@gmail.com","password2"},
+			{"email3@gmail.com","password3"}
+		};
 	}
 	@Test(priority=5,groups= {"R"})
 	public void positive_login() {

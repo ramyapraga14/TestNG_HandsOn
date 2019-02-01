d419b37d024f0a67cef999a6ead6e990d9c0d5ad
ramyapraga14 <ramya.sethu20@gmail.com>
2019-02-01 15:28:55 +0600

    changes

--- TestNG/src/PageObjects/LoginPage.java
+++ TestNG/src/PageObjects/LoginPage.java
@@ -6,5 +6,7 @@ public class LoginPage {
 	public static By email_ip= By.id("email");
 	public static By pwd_ip=By.id("pass");
 	public static By sign_in_buttn= By.id("send2");
+	public static By error_msg= By.className("error-msg");
+	public static By reg_btn= By.xpath("//span[text()='Register']");
 
 }
